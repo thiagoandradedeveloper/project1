e8d496bc053824dedbee47456686168abb95ddd1
thiagoandradedeveloper <thiagoha3@gmail.com>
2023-01-23 17:32:42 -0300

    aula 23/01/2023

---
+++ script.js
@@ -0,0 +1,83 @@
+nome  = "Lucas" //global não ultilizar
+const email = 'thiagaha3@gmail.com'
+let idade = 7 //inteiro
+var contabancaria = 15.75 //float ou real
+
+//operadores matematicos
+
+/*
+    
+    operador matematico
+
+    * multiplicação
+    / divisão
+    + soma
+    - subtração
+    % é o resto de uma divisão 5 % 2 = 1
+
+    operador de comparação
+
+    == igual
+    != diferente
+    > maior que
+    < menor que
+    >= maior ou igual
+    <= menor ou igual
+
+    operador logico 
+
+    && operador e
+    || operador ou
+
+    A & B = R
+    1 e 1 = 1
+    0 e 1 = 0
+    1 e 0 = 0
+    0 e 0 = 0
+
+    A || B = R
+    1 ou 1 = 1
+    1 ou 0 = 1
+    0 ou 1 = 1
+    0 ou 0 = 0
+
+    nome <= 'Thiago' algoritimo ou portugues estruturado
+
+    nome = 'Thiago'
+    nome := 'Thiago'
+
+    se contaBancaria >= 100 então
+        escreva(contaBancaria)
+    snao
+        escreva('Saldo Insuficiente')
+    fim_se
+
+    if(contaBancaria >= 100){
+        console.log(contaBancaria)    
+    } else {
+        
+    }
+    
+    if contaBancaria >= 100:
+        print(contaBancaria)
+    else:
+        print('sdafsadfsadfsadf')
+
+    if(contaBancaria >= 100){
+        System.out.print(contaBancaria)
+    }
+
+*/
+
+let numero = 5
+
+if(numero > 8){
+    
+    document.write('tudo ok')
+
+} else { // else significa senao
+
+    document.write('nada ok')
+
+}
+
